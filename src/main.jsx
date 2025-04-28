319d08536017b63048ba7daf7e475ccf7b63e5d4
skyline070 <yadavsumitkumar9724@gmail.com>
2025-04-28 19:17:37 +0530

    createRoot changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { Toaster } from 'react-hot-toast';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+const root = createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
     <Toaster position="top-center" />
